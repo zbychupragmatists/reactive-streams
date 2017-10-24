da6e14f35959428f6ff379a4c892fd0a99b01baa
Zbigniew Artemiuk <zbigniew.artemiuk@pragmatists.pl>
2017-10-24 16:44:03 +0200

    Schedulers

--- src/test/java/SchedulersTest.java
+++ src/test/java/SchedulersTest.java
@@ -1,10 +1,35 @@
 import org.junit.Test;
+import rx.Observable;
 
 public class SchedulersTest {
 
     @Test
     public void no_schedulers() throws Exception {
+        Utils.log("Starting");
+        Observable<String> obs = simple();
+        Utils.log("Created");
+        Observable<String> obs2 = obs
+                .map(x -> x)
+                .filter(x -> true);
+        Utils.log("Transformed");
+        obs2.subscribe(
+                s -> Utils.log(s),
+                throwable -> throwable.printStackTrace(),
+                () -> Utils.log("Completed")
+        );
+        Utils.log("Completed");
+    }
 
+    @Test
+    public void subscribe_on() throws Exception {
 
+    }
+
+    private Observable<String> simple() {
+        return Observable.unsafeCreate(subscriber -> {
+            subscriber.onNext("A");
+            subscriber.onNext("B");
+            subscriber.onCompleted();
+        });
     }
 }
