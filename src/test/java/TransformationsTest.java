8ba1ce23fca77342a70c16ce997e644f34b212af
Zbigniew Artemiuk <zbigniew.artemiuk@pragmatists.pl>
2017-10-24 15:29:10 +0200

    Flat map

--- src/test/java/TransformationsTest.java
+++ src/test/java/TransformationsTest.java
@@ -25,21 +25,59 @@ public class TransformationsTest {
     public void flatMap() throws Exception {
 
         Observable<CarPhoto> cars = cars();
-        recognize(new CarPhoto());
+        cars.flatMap(carPhoto -> recognize(carPhoto))
+                .subscribe(licensePlate -> Utils.log(licensePlate), throwable -> Utils.log(throwable));
+        Thread.sleep(300);
     }
 
     private Observable<LicensePlate> recognize(CarPhoto carPhoto) {
-        return null;
+        if (carPhoto.name.equals("A")) {
+            return Observable.just(new LicensePlate("A 123"));
+        } else if (carPhoto.name.equals("B")) {
+            return Observable.just(new LicensePlate("B 456"));
+//                    .delay(10, TimeUnit.MILLISECONDS);
+        }
+        return Observable.error(new RuntimeException("not recognised"));
     }
 
 
     private Observable<CarPhoto> cars() {
-        return null;
+        return Observable.just(
+                new CarPhoto("A"),
+                new CarPhoto("B"),
+                new CarPhoto("C")
+        );
     }
 
     private class CarPhoto {
+        private String name;
+
+        public CarPhoto(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("CarPhoto{");
+            sb.append("name='").append(name).append('\'');
+            sb.append('}');
+            return sb.toString();
+        }
     }
 
     private class LicensePlate {
+        private String number;
+
+        public LicensePlate(String number) {
+            this.number = number;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("LicensePlate{");
+            sb.append("number='").append(number).append('\'');
+            sb.append('}');
+            return sb.toString();
+        }
     }
 }
